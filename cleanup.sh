094b4d4d28908ce6ab9494155b12e8f8ead705fb
root <root@ubuntu-docker.c.portal-dev-204107.internal>
2018-11-13 06:38:04 +0000

    test-u18.04

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+source /bd_build/buildconfig
+set -x
+
+apt-get clean
+find /bd_build/ -not \( -name 'bd_build' -or -name 'buildconfig' -or -name 'cleanup.sh' \) -delete
+rm -rf /tmp/* /var/tmp/*
+
+rm -f /etc/ssh/ssh_host_*
